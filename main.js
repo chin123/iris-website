22983312809673b8cabd0dc9ded973b1b57bf7d2
Chinmaya Krishnan Mahesh <chinmaya.mahesh@disroot.org>
2018-06-27 23:27:23 +0530

    added countdown, left align rules, all caps headings

--- main.js
+++ main.js
@@ -255,3 +255,32 @@ var showold = function (ind) {
 	$("*[data-section-name=\"" + ids[ind] + "\"]").html(oldhtml[ind - 1]);
 
 }
+
+var end = new Date('07/20/2018 09:00:00 AM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+	var now = new Date();
+	var distance = end - now;
+	if (distance < 0) {
+		clearInterval(timer);
+		document.getElementById('countdown').innerHTML = 'DONE';
+		return;
+	}
+	var days = Math.floor(distance / _day);
+	var hours = Math.floor((distance % _day) / _hour);
+	var minutes = Math.floor((distance % _hour) / _minute);
+	var seconds = Math.floor((distance % _minute) / _second);
+
+	document.getElementById('countdown').innerHTML = days + ' days ';
+	document.getElementById('countdown').innerHTML += hours + ' hours ';
+	document.getElementById('countdown').innerHTML += minutes + ' minutes ';
+	document.getElementById('countdown').innerHTML += seconds + ' seconds';
+}
+
+timer = setInterval(showRemaining, 1000);
